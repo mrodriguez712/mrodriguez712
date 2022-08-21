ed3a12c6ce3398447d0c3bbd15d10b9f9bfdd80e
Marte Rodriguez <marte.rod0712@gmail.com>
2022-08-21 00:14:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const myBtn = document.getElementById("myBtn");
-const sectionTwo = document.getElementById("section-two");
+const sectionOne = document.getElementById("section-one")
 let prevScrollpos = window.pageYOffset;
 
-myBtn.addEventListener('click', function(){
-    window.scrollTo({top: 0, behavior: 'smooth'})
+myBtn.addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
 });
 
-function section() {
-    window.scrollTo({top: 0, behavior: 'smooth'})
-}
+sectionOne.addEventListener('click', function() {
+    window.scrollTo({ top: 890, behavior: 'smooth' })
+})
 
 window.onscroll = function () {
     let currentScrollPos = window.pageYOffset;
@@ -19,13 +19,44 @@ window.onscroll = function () {
         document.getElementById("navbar").style.transition = "all 1s ease-in-out"
         document.getElementById("main-body").style.transition = "all 1s ease-in-out"
         document.getElementById("main-body").style.background = "#262626"
+        document.getElementById("section-one").style.backgroundColor = "#262626"
+        document.getElementById("section-two").style.backgroundColor = "#262626"
+        document.getElementById("section-three").style.backgroundColor = "#262626"
     } else {
-        document.getElementById("navbar").style.bottom = "90%";
+        document.getElementById("navbar").style.bottom = "91%";
         document.getElementById("navbar").style.background = "#f8f8f8"
         document.getElementById("main-body").style.transition = "all 1s ease-in-out"
         document.getElementById("main-body").style.background = "#f8f8f8"
+        document.getElementById("section-one").style.backgroundColor = "#f8f8f8"
+        document.getElementById("section-two").style.backgroundColor = "#f8f8f8"
+        document.getElementById("section-three").style.backgroundColor = "#f8f8f8"
     }
     prevScrollpos = currentScrollPos;
 }
 
+// // Laptop JS
+// const mediaQuery = window.matchMedia("(min-width: 1024px) and (max-height: 1310px)")
 
+// function myFunction(mediaQuery) {
+//     if (mediaQuery.matches) {
+//         window.onscroll = function () {
+//             let currentScrollPos = window.pageYOffset;
+//             if (prevScrollpos < 650) {
+//                 document.getElementById("navbar").style.bottom = "0";
+//                 document.getElementById("navbar").style.background = "#262626"
+//                 document.getElementById("navbar").style.padding = "1em 0"
+//                 document.getElementById("navbar").style.transition = "all 1s ease-in-out"
+//                 document.getElementById("main-body").style.transition = "all 1s ease-in-out"
+//                 document.getElementById("main-body").style.background = "#262626"
+//             } else {
+//                 document.getElementById("navbar").style.bottom = "92%";
+//                 document.getElementById("navbar").style.background = "#f8f8f8"
+//                 document.getElementById("main-body").style.transition = "all 1s ease-in-out"
+//                 document.getElementById("main-body").style.background = "#f8f8f8"
+//             }
+//             prevScrollpos = currentScrollPos;
+//         }
+//     } 
+// }
+
+// myFunction(mediaQuery)
